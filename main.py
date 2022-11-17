776fb558944bb44b1c08239680696dc0edcbf044
klausmenendez <114318894+klausmenendez@users.noreply.github.com>
2022-11-16 23:00:22 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Employee:
+
+    def __init__(self, name, id_number, salary, email_address):
+        '''
+        Creates employee object with name, id number, salary and email address
+
+        '''
+        self._name = name
+        self._id_number = id_number
+        self._salary = salary
+        self._email_address = email_address
+
+    def get_name(self):  # return employee name
+        return self._name
+
+    def get_id_number(self):  # return id
+        return self._id_number
+
+    def get_salary(self):    # return employees salary
+        return self._salary
+
+    def get_email_address(self):  # returns email address
+        return self._email_address
+
+
+id_database = {}
+
+
+def make_employee_dict(names, id_numbers, salaries, email_addresses):
+    for i in range(0, len(names)):
+        id_database[id_numbers[i]] = Employee(names[i], id_numbers[i], salaries[i], email_addresses[i])
+    return id_database
+
+
+# result = make_employee_dict(["klaus", "sarah", "riley"], ["101", "102", "103"], [13.75, 15, 13.75], ["klaus@gmail.com", "sarah@gmail.com", "Riley@gmail.com"])
+# print(result["103"].get_salary())
